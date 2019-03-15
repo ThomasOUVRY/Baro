8bc374e756e0a5a7aa3578166181e3bd86930959
OUVRY Thomas <ouvrythomas@gmail.com>
2019-03-15 18:02:02 +0100

    debut java

---
+++ src/Item.java
@@ -0,0 +1,33 @@
+/** Item général, que ce soit de Baro ou Prime **/
+public class Item {
+
+	private int prix;
+	private String nom;
+
+	public Item(int prix, String nom) {
+		this.prix = prix;
+		this.nom = nom;
+	}
+
+	public int getPrix() {
+		return prix;
+	}
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [prix=" + prix + ", nom=" + nom + "]";
+	}
+
+}
